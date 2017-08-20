1f93b95a0ec9e4ad367464a5bbe28edb38bb4168
jrabramczyk <jacek.abramczyk@gmail.com>
2017-08-20 23:00:12 +0200

    First blog post

--- Personal/Personal/Personal/DataAccess/PostShortDescriptionCollection.cs
+++ Personal/Personal/Personal/DataAccess/PostShortDescriptionCollection.cs
@@ -13,8 +13,16 @@ namespace Personal.DataAccess
         {
             PostShortDescriptions = new List<PostShortDescription>
             {
-                new PostShortDescription(1, "dynamic dbContext with EF", "What if you would like to use EF and resolve object types at the runtime.", "~/Views/BlogPosts/DiAndMultipleImplementation.cshtml"),
-                new PostShortDescription(2, "DI and multi-implementation of one interface", "How to get specific implementation dynamically knowing only a type.", "~/Views/BlogPosts/DynamicDbContext.cshtml")
+                new PostShortDescription(1, "Dependency injection and custom generic type parsers - part 1",
+                                         "Lets think of a situation where you want to use some generic common mechanism for all your types to be parsed - that should be quite easy shouldn't it?" +
+                                         " But what if you would like to add custom behaviour for some of types and you really don't want to write the logic for all of them, because in most cases you still can use the generic solution." +
+                                         "<br/>Yes of course there are plenty of solutions for that kind of scenario but it will be nice to keep it clear without breaking any SOLID principle e.g. creating huge SWITCH statement for all of defined types." +
+                                         "<br/>And in that kind of scenario dependency injection container can be amazingly helpful.",
+                                         "~/Views/BlogPosts/0001_DI_GenericCustomTypeConverter.cshtml"),
+                new PostShortDescription(1, "dynamic dbContext with EF", "What if you would like to use EF and resolve object types at the runtime.",
+                                         "~/Views/BlogPosts/DiAndMultipleImplementation.cshtml"),
+                new PostShortDescription(2, "DI and multi-implementation of one interface",
+                                         "How to get specific implementation dynamically knowing only a type.", "~/Views/BlogPosts/DynamicDbContext.cshtml")
             };
         }
 
